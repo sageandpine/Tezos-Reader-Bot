bbb1a963ac38a6dc6f5e9df462659f92101e8bbd
pc84 <61305293+sageandpine@users.noreply.github.com>
2021-12-27 04:29:56 +0000

    revised query to increase data pull to 50 objkts. 'tag' filter on original was limiting number of objkts being fetched. also initiated database function for recording what objkts have been calle dby the bot. -pc84

--- main.py
+++ main.py
@@ -2,6 +2,7 @@
 # Tezos_Reader_Bot pulls a random pdf NFT from hic dex and displays it in discord chat with a link and info when 
 # $read is called.
 # Add more commands
+#TO DO: Watch for Repeat objects
 #TO DO: Retrieve spoken word?
 #To Do: Filter by tag
 # To Do: Rate Limiting
@@ -18,17 +19,20 @@ import pandas as pd
 my_secret = os.environ['bot-tots']
 client  = discord.Client()
 
-poetry = "poetry"
-fiction = "fiction"
-sci-fi = "sci-fi"
-comic = "comic"
+# poetry = "poetry"
+# fiction = "fiction"
+# sci-fi = "sci-fi"
+# comic = "comic"
 
 
-main_q = """query ObjktsByTag($tag1: String_comparison_exp = {_eq: "fiction"}) {
-  hic_et_nunc_token(where: {mime: {_in: "application/pdf"}, token_tags: {tag: {tag: $tag1}}}, limit: 10) {
+main_q = """query PdfObjkts {
+  hic_et_nunc_token(where: {mime: {_in: "application/pdf"}}, limit: 50) {
+    mime
+    title
     id
   }
 }
+
 """
 def get_rand_pdf(number):
   """Returns a Random NFT PDF from the HicDex API."""
@@ -42,10 +46,18 @@ def get_rand_pdf(number):
   #Access token number and store in variable
   df_objkt_id = df["data"]["hic_et_nunc_token"][number]["id"]
   # Format into a string to be returned by function
-  #link_string = f"https://hen.radio/objkt/{df_objkt_id}"
   link_string = f"https://hic.art/{df_objkt_id}"
-  print(link_string)
   return link_string
+
+def update_objkt_list(objkt_number):
+  """Updates the replit DataBase and stores all NFTs called by the readerbot"""
+  if "objkt" in db.keys():
+    objkt = db["objkt"]
+    objkt.append(objkt_number)
+    db["objkt"] = objkt
+    
+  else:
+    db["objkt"] = [objkt_number]
 
 
 @client.event
@@ -60,9 +72,9 @@ async def on_message(message):
     return
 
   if message.content.startswith('$read'):
-    num = random.randrange(0,10)
+    num = random.randrange(0,50)
     rand_pdf = get_rand_pdf(num)
-    #update_objkt_list(rand_music) 
+    update_objkt_list(rand_pdf) 
     await message.channel.send(rand_pdf)
 
 keep_alive()
